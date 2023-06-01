75f1328fe4cd300345bf353403fe56d8fe0dba64
Gustavo Gussoni <gussonigq@gmail.com>
2023-06-01 11:07:45 -0300

    feat: create swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Clients App')
+    .setDescription(
+      'This API was build to create clients accounts and add contacts to it',
+    )
+    .setVersion('1.0')
+    .addTag('Login')
+    .addTag('Clients')
+    .addBearerAuth()
+    .addTag('Contacts')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
     new ValidationPipe({
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,17 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { LoginDTO } from './dto/login.dto';
 
-interface IClientLogin {
-  email: string;
-  password: string;
-}
-
+@ApiTags('Login')
 @Controller('login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('')
   @UseGuards(LocalAuthGuard)
-  async login(@Body() client: IClientLogin) {
+  async login(@Body() client: LoginDTO) {
     return this.authService.login(client.email);
   }
 }
